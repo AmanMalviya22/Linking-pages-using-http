cfb6d4ec358f7ecb1f204a4d2a7b78d95acbd455
AmanMalviya22 <amanaman26802@gmail.com>
2023-06-13 16:41:53 +0530

    updated code for serving different html files

--- basichttpserver/index.js
+++ basichttpserver/index.js
@@ -1,43 +1,39 @@
-const http=require("http");
-const port=8000;
-const fs=require('fs');
-const t=require('buffer');
-function requestHandler(req,res){
-   // console.log(req.url);
-   res.writeHead(200,{'Content-type':'text/html'});
-  //  res.end('<h1>Gotcah!</h1>');
+const http = require("http");
+const port = 8000;
+const fs = require("fs");
 
+function requestHandler(req, res) {
+  res.writeHead(200, { "Content-type": "text/html" });
 
+  let path;
+  switch (req.url) {
+    case "/":
+      path = "./index.html";
+      break;
+    case "/signup": // corrected the path
+      path = "./signup.html";
+      break;
+    default:
+      path = "./404.html";
+      break;
+  }
 
-   fs.readFile('./index.html', function(err,data){
-     if(err){
-        console.log("error",err);
-        return res.end('<h1>Error</h1>');
-     }
-     return res.end(data);
-   })
-   fs.readFile('./home.html', function(err,data){
-      if(err){
-         console.log("error",err);
-         return res.end('<h1>Error</h1>');
-      }
-      return res.end(data);
-    })
-
+  fs.readFile(path, function (err, data) {
+    // corrected the path variable
+    if (err) {
+      console.log("error", err);
+      return res.end("<h1>Error</h1>");
+    }
+    return res.end(data);
+  });
 }
 
-const server=http.createServer(requestHandler);
-//create server
+const server = http.createServer(requestHandler);
 
-
-server.listen(port,function(err){
-   if(err){
-       console.log(err);
-       return;
-   }
-   console.log("server is up and running on post",port);
-})
-
-
-
-
+server.listen(port, function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("server is up and running on port", port);
+});
