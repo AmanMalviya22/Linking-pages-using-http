76dc1e882a2a55e2635c0a23438c0ce1fb46b2ef
AmanMalviya22 <amanaman26802@gmail.com>
2023-06-16 10:10:53 +0530

    created constactlist and passed it to ejs template

--- contact-list-app/index.js
+++ contact-list-app/index.js
@@ -1,32 +1,38 @@
-const express=require('express');
-const path=require('path');
-const port=8000;
+const express = require("express");
+const path = require("path");
+const port = 8000;
 
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-
-const app=express();
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
-
-
+let contactList = [
+  {
+    name: "aman",
+    age: 20,
+  },
+  {
+    name: "rohit",
+    age: 25,
+  },
+  {
+    name: "monika",
+    age: 23,
+  },
+];
 
 //created controller for home route or entry point
-app.get('/',function(req,res){
-    
-    res.render('home',{title:"my express app"});
+app.get("/", function (req, res) {
+  res.render("home", { title: "my express app" ,contact_list:contactList});
 });
 
-app.get('/practice',function(req,res){
-    res.render('practice',{ title:'my practice app'});
+app.get("/practice", function (req, res) {
+  res.render("practice", { title: "my practice app" });
 });
 
-
-
-
-app.listen(port,function(err){
-    if(err){
-        console.log('error in running the server',err);
-    }
-    console.log("server is started");
-
-})
+app.listen(port, function (err) {
+  if (err) {
+    console.log("error in running the server", err);
+  }
+  console.log("server is started");
+});
