5a85701a5ae25918c561cb84d24099190e78bb0f
AmanMalviya22 <amanaman26802@gmail.com>
2023-06-17 12:14:50 +0530

    learn middleware || create our own middleware ||

--- contact-list-app/index.js
+++ contact-list-app/index.js
@@ -2,9 +2,31 @@ const express = require("express");
 const path = require("path");
 const port = 8000;
 
+//app has all functionality of express
 const app = express();
+//setup out view engine as ejs
 app.set("view engine", "ejs");
+//set up views folder and used it in current file
 app.set("views", path.join(__dirname, "views"));
+//adding the middleware, you can access the parsed form data in the req.body object within your route handlers
+app.use(express.urlencoded({ extended: true }));
+
+// created our own middleware1
+app.use(function(req,res,next){
+console.log("middleware 1 is called");
+req.myname="rahul";
+// calling next middleware
+next();
+})
+
+// //created our own middleware2
+app.use(function(req,res,next){
+  console.log("myname form middleware 2",req.myname);
+  console.log("middleware 2 is called");
+  //calling next middleware
+  next();
+
+  })
 
 let contactList = [
   {
@@ -23,18 +45,23 @@ let contactList = [
 
 //created controller for home route or entry point
 app.get("/", function (req, res) {
-  res.render("home", { title: "my express app" ,contact_list:contactList});
+  console.log("my name frome home route",req.myname);
+  res.render("home", { title: "my express app", contact_list: contactList });
 });
 
+//controller for practice route
 app.get("/practice", function (req, res) {
   res.render("practice", { title: "my practice app" });
 });
-app.post('/create-contact',function(req,res){
-    var data=req.body;
-    console.log(data.name);
-    console.log(data.phone);
-    return res.redirect('/practice');
-})
+
+//controller for create-contact route
+app.post("/create-contact", function (req, res) {
+  contactList.push({
+    name: req.body.name,
+    age: req.body.age, // Add the phone property
+  });
+  res.redirect("back");
+});
 
 app.listen(port, function (err) {
   if (err) {
